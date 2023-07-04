713f5a63dee970423ae474a6c3bd8dad3250fb3d
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-07-04 17:21:46 +0800

    added is_game_on

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ from paddle import Paddle
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.title("Pong")
 screen.tracer(0)
+player_one = Paddle((-350, 0))
+player_two = Paddle((350, 0))
 
-player_one = Paddle(-380, 0)
-player_two = Paddle(380, 0)
-screen.update()
-
-screen.listen()
+# Player One Controls
 screen.onkey(player_one.move_up, "W")
 screen.onkey(player_one.move_up, "w")
 screen.onkey(player_one.move_down, "S")
 screen.onkey(player_one.move_down, "s")
+
+# Player Two Controls
+screen.onkey(player_two.move_up, "Up")
+screen.onkey(player_two.move_down, "Down")
+
+
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    screen.listen()
+    screen.tracer(1)
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -3,19 +3,19 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self, xcor, ycor):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.shapesize(stretch_len=5, stretch_wid=1)
         self.color("white")
         self.penup()
-        self.goto(xcor, ycor)
+        self.goto(position)
         self.setheading(90)
 
     def move_up(self):
         print("Moving Up")
-        self.forward(100)
+        self.forward(20)
 
     def move_down(self):
         print("Moving Down")
-        self.backward(100)
+        self.backward(20)
